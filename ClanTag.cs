50a6c83913f542ec93bf061e6f1e807a6becdf0c
INSANEMODE <scottshome02@gmail.com>
2020-11-20 04:05:14 -0600

    update version, and add support for renaming default rank names through ClanTagCommands.json

--- ClanTag.cs
+++ ClanTag.cs
@@ -1,52 +1,61 @@
 ﻿
 using SharedLibraryCore.Database.Models;
-
+using SharedLibraryCore;
+using SharedLibraryCore.Interfaces;
 
 namespace ClanTagRankApi
 {
-    public static class ExtensionMethods
+
+    static class ExtensionMethods
     {
-        public static string ClanTag(this EFClient.Permission level)
+        public static string Truncate(this string input, int strLength)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input.Length <= strLength ? input : input.Substring(0, strLength);
+        }
+        public static string ClanTag(this EFClient.Permission level, Configuration Config)
         {
             string rankName;
+
             switch ((int)level)
             {
                 case -1:
                     rankName = "Banned";  //this typically won't be seen. 
                     break;
                 case 0:
-                    rankName = "User";
+                    rankName = Config.User;
                     break;
                 case 1:
-                    rankName = "User";  //1 = flagged, but don't want to show this in game.
+                    rankName = Config.User;  //1 = flagged, but don't want to show this in game.
                     break;
                 case 2:
-                    rankName = "Trusted";
+                    rankName = Config.Trusted;
                     break;
                 case 3:
-                    rankName = "Mod";
+                    rankName = Config.Moderator;
                     break;
                 case 4:
-                    rankName = "Admin";
+                    rankName = Config.Admin;
                     break;
                 case 5:
-                    rankName = "SrAdmin";
+                    rankName = Config.SeniorAdmin;
                     break;
                 case 6:
-                    rankName = "Owner";
+                    rankName = Config.Owner;
                     break;
                 case 7:
-                    rankName = "Creator";
+                    rankName = Config.Creator;
                     break;
                 case 8:
-                    rankName = "Console";
+                    rankName = Config.Console;
                     break;
                 default:
-                    rankName = "User";
+                    rankName = Config.User;
                     break;
             }
-            return rankName;
+            return rankName.Truncate(8);
         }
+
 
 
     }
